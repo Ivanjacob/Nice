35ef924affcb80467096e7a957a4579b5dc8e639
Ivanjacob <evansjacobmurimi@gmail.com>
2022-06-24 02:45:34 +0300

    added test_msisdn

---
+++ keys.py
@@ -0,0 +1,8 @@
+
+business_shortCode = "174379" # Lipa Na Mpesa ShortCode
+phone_number = "254759656856"
+lipa_na_mpesa_passkey = "bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919"
+consumer_key = "TIAlApOeTFdiE5HAtQBBe8Zib2nvmLeO"
+consumer_secret = "WrawARG7arK7R6OZ"
+shortcode = "601431"
+test_msisdn = "25408374149"
