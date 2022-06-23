6eec19618ab5633cc556279df3cbddebe4c4084c
Ivanjacob <evansjacobmurimi@gmail.com>
2022-06-24 02:43:15 +0300

    added simulate_c2b_transaction and register_url

---
+++ c2b.py
@@ -0,0 +1,45 @@
+from requests.auth import HTTPBasicAuth
+import requests
+
+from access_token import generate_access_token
+import keys
+
+
+def register_url():
+    my_access_token = generate_access_token()
+    api_url = "https://sandbox.safaricom.co.ke/mpesa/c2b/v1/registerurl"
+
+    headers = {"Authorization": "Bearer %s" % my_access_token}
+
+    request = {
+        "ShortCode":keys.shortcode,
+        "ResponseType": "Completed",
+        "ConfirmationURL": "https://nicerice.com/confirmation",
+        "ValidationURL": "https://nicerice.com/validation"}
+
+    response = requests.post(api_url, json = request, headers=headers)
+
+    print (response.text)
+
+# register_url()
+
+def simulate_c2b_transaction():
+    my_access_token = generate_access_token()
+
+    api_url = "https://sandbox.safaricom.co.ke/mpesa/c2b/v1/simulate"
+
+    headers = {"Authorization": "Bearer %s" % my_access_token}
+
+    request = {
+        "ShortCode":keys.shortcode,
+        "CommandID": "CustomerPayBillOnline",
+        "Amount": "2",
+        "Msisdn":keys.test_msisdn,
+        "BillRefNumber":"12345678"
+        }
+
+    response = requests.post(api_url, json = request, headers = headers)
+
+    print (response.text)
+
+simulate_c2b_transaction()
